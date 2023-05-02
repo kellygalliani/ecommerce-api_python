ae890e1fe31eb20df201faa3ef6bb5bd53e1beda
silvadpablo <teacherpablod@hotmail.com>
2023-05-02 14:11:45 -0300

    feat: created product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Product(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    stock = models.IntegerField()
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    availability = models.BooleanField()
+    seller = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="products"
+    )
