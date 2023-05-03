d280c2f6cc5a043c22783135b1b0e90a2c59615d
silvadpablo <teacherpablod@hotmail.com>
2023-05-03 13:53:39 -0300

    feat: created product views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+
+class ProductReadAllView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        return serializer.save(seller_id=self.request.user.id)
+
+
+class ProductDetailsView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
