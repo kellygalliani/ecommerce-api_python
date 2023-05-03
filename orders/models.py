4e3ffa78c770946c5f8d83cfd4abbd648890c724
Gustavo Gussoni <gussonigq@gmail.com>
2023-05-03 09:57:29 -0300

    feat: create order model

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class OrderStatusOptions(models.TextChoices):
+    order_started = "PEDIDO REALIZADO"
+    order_in_progess = "EM ANDAMENTO"
+    delivered = "ENTREGUE"
+
+
+class Order(models.Model):
+    class Meta:
+        ordering = ["id"]
+
+    order_status = models.CharField(
+        choices=OrderStatusOptions.choices,
+        default=OrderStatusOptions.order_started,
+    )
+    total_price = models.DecimalField(
+        max_digits=9, decimal_places=2, default=0.00
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        "users.User", on_delete=models.PROTECT, related_name="orders"
+    )
