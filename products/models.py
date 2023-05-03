99cab27e8b0a7135cd02e84d72b01d40d1afcb37
silvadpablo <teacherpablod@hotmail.com>
2023-05-03 08:58:55 -0300

    fix: updated product to reflect corrections

--- products/models.py
+++ products/models.py
@@ -1,13 +1,11 @@
 from django.db import models
-import uuid
+
 
 class Product(models.Model):
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     stock = models.IntegerField()
     price = models.DecimalField(max_digits=9, decimal_places=2)
-    availability = models.BooleanField()
     seller = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="products"
     )
--- products/serializers.py
+++ products/serializers.py
@@ -6,26 +6,35 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
-            "id",
             "name",
             "category",
             "stock",
             "price",
             "availability"
+            "seller_id"
         ]
+        read_only_fields = ["id"]
 
         def create(self, validated_data):
             return Product.objects.create(**validated_data)
+
+        def update(self, instance, validated_data):
+            for key, value in validated_data.items():
+                setattr(instance, key, value)
+            
+            instance.save()
+
+            return instance
 
 
 class ProductInCartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
-            "id",
             "name",
             "category",
             "price",
-            "availability",
+            "seller_id"
         ]
+        read_only_fields = ["id"]
         exclude = ["stock"]
