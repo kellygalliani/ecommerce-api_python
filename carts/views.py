778ccffbff6f814deb7b6c3cb0f4452318d84d8f
silvadpablo <teacherpablod@hotmail.com>
2023-05-04 09:40:29 -0300

    fix: product views update

--- carts/views.py
+++ carts/views.py
@@ -8,9 +8,10 @@ from .models import Cart
 from .serializers import CartSerializer
 
 
-class CartView(generics.RetrieveUpdateAPIView):
+class CartView(generics.UpdateAPIView):
     serializer_class = CartSerializer
     permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
 
     def get_queryset(self):
         return Cart.objects.filter(user=self.request.user)
--- products/views.py
+++ products/views.py
@@ -1,14 +1,16 @@
 from rest_framework import generics
 from .serializers import ProductSerializer
 from .models import Product
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
 class ProductReadAllView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def perform_create(self, serializer):
-        return serializer.save(seller_id=self.request.user.id)
+        return serializer.save(seller=self.request.user)
 
 
 class ProductDetailsView(generics.UpdateAPIView):
