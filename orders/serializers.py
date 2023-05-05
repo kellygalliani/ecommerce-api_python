72358b88c3a8e91d2f81ef0ccc95ac5b91813f6b
Gustavo Gussoni <gussonigq@gmail.com>
2023-05-05 14:22:10 -0300

    fix order view

--- orders/serializers.py
+++ orders/serializers.py
@@ -20,50 +20,39 @@ class OrderSerializer(serializers.ModelSerializer):
         depth = 2
 
     def create(self, validated_data: dict) -> Order:
-        # print("-" * 80, validated_data["user"])
-        # print("-" * 80, validated_data)
-        # print("-" * 80, validated_data["user"].cart.id)
-        # print(validated_data.get("user").cart.products)
-        cart_products = validated_data["user"].cart.products
         cart = Cart.objects.get(id=validated_data["user"].cart.id)
-
-        product_in_cart = CartProducts.objects.filter(
-            cart_id=validated_data["user"].cart.id
-        ).first()
-        print("-" * 80, product_in_cart)
 
         sellers = []
         for item in cart.cartproducts_set.all():
-            # print(item.product_id)
             product = Product.objects.get(id=item.product_id)
-            # print(product.seller_id)
+
             if product.seller_id not in sellers:
                 sellers.append(product.seller_id)
-        orders = []
+
         for seller in sellers:
             products_list = []
+            quantity = 0
+            total_price = 0
             for item in cart.cartproducts_set.all():
                 product = Product.objects.get(id=item.product_id)
                 if product.seller_id == seller:
                     products_list.append(product)
-            # ipdb.set_trace()
-            # print(products_list)
+                    quantity += item.quantity
+                    total_price += product.price * item.quantity
+
             order = Order.objects.create(**validated_data)
             order.product.set(products_list)
-            # order_id = order.id
-            # order_made = Order.objects.get(id=order_id)
-            # orders.append(order_made)
+            order.total_price = total_price
+            order.save()
 
-            # print(order.id)
-        # for order in orders:
-        # order_product = OrderProducts.objects.get(order_id=order.id)
+            order_products = OrderProducts.objects.filter(order_id=order.id)
 
-        # ipdb.set_trace()
-        # order.user = user
+            for order in order_products:
+                order.quantity = quantity
 
-        # order.product.set(cart_products.all())
-        # return OrderProducts.objects.get(id=)
-        return self
+                order.save()
+
+        return order
 
     def update(self, instance: Order, validated_data: dict) -> Order:
         for key, value in validated_data.items():
