5450c3ab793d1e735df254ba54fe1ad0d149be85
kelly galliani <kelly_92b@hotmail.com>
2023-05-05 09:04:34 -0300

    feat: permissions updated

--- addresses/views.py
+++ addresses/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
-from users.permissions import IsAccountOwnerOrAdmin
+from .permissions import IsAccountOwnerOrAdmin
 from .models import Address
 from .serializers import AddressSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -14,9 +13,9 @@ class AddressUpdateView(generics.UpdateAPIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAccountOwnerOrAdmin]
     
-    def get_queryset(self):
+    """ def get_queryset(self):
         if not self.request.user.is_authenticated:
             raise NotAuthenticated("Authentication credentials were not provided.")
-        return Address.objects.filter(pk=self.kwargs["pk"], user=self.request.user)
+        return Address.objects.filter(pk=self.kwargs["pk"], user=self.request.user) """
 
     
