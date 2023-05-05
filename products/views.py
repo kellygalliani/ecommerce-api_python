98bfbcc19fef1d9133373ce78642c829b9f24c6f
kelly galliani <kelly_92b@hotmail.com>
2023-05-05 08:40:17 -0300

    feat: permissions

--- products/views.py
+++ products/views.py
@@ -1,18 +1,34 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .serializers import ProductSerializer
 from .models import Product
 from rest_framework_simplejwt.authentication import JWTAuthentication
-
+from users.permissions import IsProductSellerOrAdmin
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import AnonymousUser
+from rest_framework.exceptions import PermissionDenied
 
 class ProductReadAllView(generics.ListCreateAPIView):
     authentication_classes = [JWTAuthentication]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsProductSellerOrAdmin]
+
+    def get_permissions(self):
+            if self.request.method in permissions.SAFE_METHODS:
+                return [AllowAny()]
+            return super().get_permissions()
 
     def perform_create(self, serializer):
+        if self.request.user == AnonymousUser():
+             raise PermissionDenied("Authentication credentials were not provided.")
+        
         return serializer.save(seller=self.request.user)
-
-
+    
 class ProductDetailsView(generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsProductSellerOrAdmin]
+    authentication_classes = [JWTAuthentication]
+
+
+
--- users/views.py
+++ users/views.py
@@ -4,14 +4,32 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer
 from rest_framework import generics
 from drf_spectacular.utils import extend_schema
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
+from .permissions import IsAccountOwnerOrAdmin
 
-class UserView(generics.CreateAPIView):
+class UserView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
 
+    def get_permissions(self):
+            if self.request.method =='POST':
+                return [AllowAny()]
+            return super().get_permissions()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.is_superuser:
+            return User.objects.all()
+        else: 
+             user_id = self.request.user.id
+             return queryset.filter(id=user_id)
+    
 
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     authentication_classes = [JWTAuthentication]
-    """ permission_classes =  """
+    permission_classes = [IsAccountOwnerOrAdmin]
