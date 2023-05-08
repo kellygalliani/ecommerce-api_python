6b82c779bff31b7900660ca68a83c590d6063ee0
silvadpablo <teacherpablod@hotmail.com>
2023-05-08 09:16:36 -0300

    fix: adjusted user details url

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path("users/", views.UserView.as_view()),
-    path("users/<int:pk>/", views.UserDetailView.as_view()),
-    path("users/<int:pk>/seller", views.UserDetailView.as_view()),
+    path("users/<uuid:pk>/", views.UserDetailView.as_view()),
+    path("users/<uuid:pk>/seller", views.UserDetailView.as_view()),
     path("users/login/", TokenObtainPairView.as_view()),
 ]
